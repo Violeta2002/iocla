1e53991eab09c4333ff15589b73c45588bf6da24
Violeta2002 <comsa.iulianavioleta@gamil.com>
2022-03-08 00:22:55 +0200

    Laborator 1: exercitiul 2: Add solution for exercise 2
    Simple strlen implementation using the fact that
    string in c are terminated with 0.
    
    Signed-off-by: Violeta2002 <comsa.iulianavioleta@gamil.com>

--- laborator/content/reprezentare-numere/2-len_xor/len_xor.c
+++ laborator/content/reprezentare-numere/2-len_xor/len_xor.c
@@ -4,22 +4,36 @@
 
 int my_strlen(const char *str)
 {
-	/* TODO */
-	(void) str;
-
-	return -1;
+	int len = 0;
+	const char *p = str;
+	
+	while(*p) {
+		len++;
+		p++;
+	}
+	return len;
 }
 
 void equality_check(const char *str)
 {
-	/* TODO */
-	(void) str;
+	const char *p = str;
+	int i = 0;
+	int len = my_strlen(str);
+	while(*(p+i))
+	{
+		if(!(*(p+i) ^ *(p+(i+(1<<i))%len)))
+			printf("%p\n", p+i);
+		i++;
+	}
 }
 
 int main(void)
 {
-	/* TODO: Test functions */
-
+	char str[256];
+	scanf("%s", str);
+	int len = my_strlen(str);
+	printf("%d\n", len);
+	equality_check(str);
 	return 0;
 }
 
