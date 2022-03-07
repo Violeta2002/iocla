18ed1758944319547907737dfa7a3e7d7bcce36b
Violeta2002 <comsa.iulianavioleta@gamil.com>
2022-03-08 00:34:03 +0200

    Laboratorul 1: exercitiul 5: Add solutions for exercise 5
    I go through an array of integers and print binary if the
    number is even and hexadecimal if the number is odd.
    
    Signed-off-by: Violeta2002 <comsa.iulianavioleta@gamil.com>

--- laborator/content/reprezentare-numere/5-odd_even/odd_even.c
+++ laborator/content/reprezentare-numere/5-odd_even/odd_even.c
@@ -3,22 +3,33 @@
 
 void print_binary(int number, int nr_bits)
 {
-	/* TODO */
-	(void) number;
-	(void) nr_bits;
+	for(int c = nr_bits; c >= 0; c--) {
+		int k = number >> c;
+	if( k & 1)
+		printf("1");
+	else
+		printf("0");
+	}
+	printf("\n");
 }
 
 void check_parity(int *numbers, int n)
 {
-	/* TODO */
-	(void) numbers;
-	(void) n;
+	for(int i = 0; i < n; i++)
+		if((*(numbers + i) ^ 1 )== *(numbers +i) +1)
+			print_binary(*(numbers + i), 8 * sizeof(int));
+		else 
+			printf("0x%x\n", *(numbers + i));
 }
 
 int main()
 {
-	/* TODO: Test functions */
-
+	int n, *numbers;
+	scanf("%d", &n);
+	numbers = malloc(n * sizeof(int));
+	for(int i = 0; i < n; i++)
+		scanf("%d", numbers+i);
+	check_parity(numbers, n);
 	return 0;
 }
 
