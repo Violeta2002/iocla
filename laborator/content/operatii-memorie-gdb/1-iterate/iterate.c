6b1a7cc6e3920a9215b6c317d32f36920f1a1f0d
Violeta2002 <comsa.iulianavioleta@gamil.com>
2022-03-14 16:56:12 +0200

    Laborator2: exercitiul 1: Add solutions for execise 1
    I used 3 functions for print: print_char, print_short, print_int
    that goes through an array up to sizeof(int)/ {sizeof(char), sizeof(short),
    sizeof(int)} and prints the value and the address of a specific element
    
    Signed-off-by: Violeta2002 <comsa.iulianavioleta@gamil.com>

--- laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
+++ laborator/content/operatii-memorie-gdb/1-iterate/iterate.c
@@ -6,11 +6,35 @@
  * Parcurgeti adresele, pe rand, din octet in octet,
  * din 2 in 2 octeti si apoi din 4 in 4.
  */
+void print_char(int *v, int n)
+{
+    unsigned char *p =(unsigned char *)v;
+    for(unsigned char i = 0; i < sizeof(int)/sizeof(char)*n; i++)
+        printf("%p -> 0x%hhx\n", p+i, *(p+i));
+}
 
+void print_short(int *v, int n)
+{
+    short *p = (short *)v;
+    for(unsigned short i = 0; i < sizeof(int)/sizeof(short) * n; i++)
+          printf("%p -> 0x%hhx\n", p+i, *(p+i));
+}
+
+void print_int(int *v, int n)
+{
+    int *p = v;
+    for( int i = 0; i < n; i++)
+        printf("%p -> 0x%hhx\n", p+i, *(p+i));
+}
 int main() {
     int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
+    printf("Print char:\n");
+    print_char(v,5);
 
-    (void) v;
+    printf("Print short:\n");
+    print_short(v,5);
 
+    printf("Print int:\n");
+    print_int(v,5);
     return 0;
 }
