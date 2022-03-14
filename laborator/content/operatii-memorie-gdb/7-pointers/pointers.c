7530c19803c6498fc08024541b45fa4a829e3eb4
Violeta2002 <comsa.iulianavioleta@gamil.com>
2022-03-14 23:18:47 +0200

    Laborator2: exercise 7: Add solutions for exercise 7 I only implemented my_strcpy and my_strcmp functions using pointers and the value they point to. Signed-off-by: Violeta2002 <comsa.iulianavioleta@gamil.com>

--- laborator/content/operatii-memorie-gdb/7-pointers/pointers.c
+++ laborator/content/operatii-memorie-gdb/7-pointers/pointers.c
@@ -3,9 +3,43 @@
 #include <assert.h>
 #include <stdlib.h>
 
-int my_strcmp(const char *s1, const char *s2);
+int my_strcmp(const char *s1, const char *s2)
+{
+	while(*s1) {
+	if(*s1 != *s2)
+		return 1;
+	
+	s1++;
+	s2++;	
+	}
+	return 0;
+}
+
 void *my_memcpy(void *dest, const void *src, size_t n);
-char *my_strcpy(char *dest, const char *src);
+
+	/*while(n--) {
+		*(char *)dest = *(char *)src;
+		dest++;
+		src++;
+		//if(*(char *)(dest++) == '\0')
+			//(char *)dest = (char *)realloc(dest, (strlen(dest) + 1) * sizeof(char));
+	}
+	*/
+
+
+char *my_strcpy(char *dest, const char *src)
+{
+	char *p = dest;
+
+	while(*src != '\0') {
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+	return p;
+}
 
 int main() {
 	char s1[] = "Abracadabra";
@@ -21,9 +55,11 @@ int main() {
 	Daca functia voastra este implementata corect atunci asertia se va realiza
 	cu succes. In caz contrar, programul va crapa.
 	*/
-	// assert(my_strcmp(s1, s2) == strcmp(s1, s2));
-	// assert(my_memcpy(dest, src, sizeof(src)) == memcpy(dest, src, sizeof(src)));
-	// assert(my_strcpy(dest, src) == strcpy(dest, src));
+	assert(my_strcmp(s1, s2) == strcmp(s1, s2));
+		printf("Those 2 string functions are the same\n");
+	assert(my_memcpy(dest, src, sizeof(src)) == memcpy(dest, src, sizeof(src)));
+	assert(my_strcpy(dest, src) == strcpy(dest, src));
+		printf("my_strcpy and strcpy do the same thing");
 	free(dest);
 
 	return 0;
