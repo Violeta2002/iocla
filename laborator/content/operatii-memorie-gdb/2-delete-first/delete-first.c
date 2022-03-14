a076f21fe94d84061794fa490b90ca4c51edc323
Violeta2002 <comsa.iulianavioleta@gamil.com>
2022-03-14 17:03:11 +0200

    Laborator2: exercise 2: Add solution for exercise 2
    I deleted the first occurrence of the pattern by finding out
    the position from which it starts with the strstr function, then
    i copied over the initial string + the position from which the
    pattern starts, the initial string from the pattern position + strlen(pattern)
    
    Signed-off-by: Violeta2002 <comsa.iulianavioleta@gamil.com>

--- laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
+++ laborator/content/operatii-memorie-gdb/2-delete-first/delete-first.c
@@ -2,17 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* delete_first(char *s, char *pattern);
+char* delete_first(char *s, char *pattern)
+{
+	int p;
+	if(strstr(s , pattern))
+		p = strstr(s,pattern) - s;
+	char *s2 = strcpy(s + p, s + p + strlen(pattern));
+	memcpy(s + p, s2, strlen(s2) );
+	return s;
+}
 
 int main(){
-	char *s = "Ana are mere";
+	char s[] = "Ana are mere";
 	char *pattern = "re";
 
-	(void) s;
-	(void) pattern;
-
 	// Decomentati linia dupa ce ati implementat functia delete_first.
-	// printf("%s\n", delete_first(s, pattern));
+	 printf("%s\n", delete_first(s, pattern));
 
 	return 0;
 }
