ef0b91db05f06a60477f6d3b24cf47d9e084ca43
Violeta2002 <comsa.iulianavioleta@gamil.com>
2022-04-07 19:02:09 +0300

    Laborator5: exercise 6: Add solutions for exercise 6
    I calculated the nth fibonacci number using instruction such as
    mov, add and jump.
    
    Signed-off-by: Violeta2002 <comsa.iulianavioleta@gamil.com>

--- laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
+++ laborator/content/introducere-asamblare/6-fibonacci/fibonacci.asm
@@ -8,5 +8,19 @@ main:
     mov eax, 7       ; vrem sa aflam al N-lea numar; N = 7
 
     ; TODO: calculati al N-lea numar fibonacci (f(0) = 0, f(1) = 1)
+    mov ebx, 0
+    mov ecx, 1
 
+fibo:
+    cmp eax, 2
+    je print
+    mov edx, ebx
+    add edx, ecx 
+    mov ebx, ecx
+    mov ecx, edx 
+    add eax, -1
+    jmp fibo
+
+print:
+    PRINTF32 `%d\n\x0`, edx
     ret
